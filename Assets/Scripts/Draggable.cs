6bfb768dddd92f1d6042b5cd4df404874a2d4b26
Tetawex <tetawex@gmail.com>
2021-12-23 22:14:36 +0300

    Add scene and scripts

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 // TODO: Add callbacks for sounds and stuff
 public class Draggable : MonoBehaviour
 {
+    public static float SNAP_RANGE = 0.5f;
+
     private DraggableSlot? previousSlot = null;
 
     // TODO: Refactor to enum state, add a state for animated snapping
@@ -20,10 +22,10 @@ public class Draggable : MonoBehaviour
 
     }
 
-    private void snapToSlot(DraggableSlot? slot)
+    public void SnapToSlot(DraggableSlot? slot)
     {
         var slotToSnapTo = slot ?? previousSlot;
-        if (slotToSnapTo is not null)
+        if (slotToSnapTo != null)
         {
             transform.position = slot.GetPosition();
         }
@@ -35,13 +37,20 @@ public class Draggable : MonoBehaviour
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        transform.position = mousePos;
+        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
     }
 
     private void setDragged(bool isDragged)
     {
         this.isDragged = isDragged;
 
+        if (this.isDragged == false)
+        {
+            var result = Physics2D.OverlapCircle(transform.position, SNAP_RANGE);
+            var nearestPoint = player.transform.position.NearestOf
+            //var closestSlot = gameObject.
+            //snapToSlot
+        }
     }
 
     public void OnMouseDown()
