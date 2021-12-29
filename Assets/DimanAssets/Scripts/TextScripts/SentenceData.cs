3fe8b47bf564ac4a93c65d35406347dbf844ba41
Dmitriy Borovkov <maxwaeper@gmail.com>
2021-12-29 17:24:27 +0300

    Unfinished Pilot select

--- Assets/DimanAssets/Scripts/TextScripts/SentenceData.cs
+++ Assets/DimanAssets/Scripts/TextScripts/SentenceData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Data", menuName = "Sentences", order = 1)]
-public class SentenceData :ScriptableObject
+public class SentenceData : ScriptableObject
 {
     [SerializeField]
     public List<Sentence> Sentences;
