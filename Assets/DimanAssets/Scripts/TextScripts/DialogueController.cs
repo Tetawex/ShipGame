f0b2175354297499de30cf4c27f4b6aa22ce4769
Dmitriy Borovkov <maxwaeper@gmail.com>
2021-12-30 17:46:57 +0300

    Dialogue response system

--- Assets/DimanAssets/Scripts/TextScripts/DialogueController.cs
+++ Assets/DimanAssets/Scripts/TextScripts/DialogueController.cs
@@ -7,6 +7,11 @@ public class DialogueController : MonoBehaviour
 {
     public Text nameText;
     public Text dialogueText;
+    public Text Response1;
+    public Text Response2;
+    public Text Response3;
+    public List<Text> ResponseBoxes;
+    
 
     private Queue<Sentence> currentSentences;
 
@@ -15,7 +20,9 @@ public class DialogueController : MonoBehaviour
 
     private void Start()
     {
-
+        ResponseBoxes.Add(Response1);
+        ResponseBoxes.Add(Response2);
+        ResponseBoxes.Add(Response3);
     }
 
     public void StartDialogue()
@@ -26,23 +33,43 @@ public class DialogueController : MonoBehaviour
 
     public void EndDialogue()
     {
-
+        
     }
 
     public void DisplayNextSentence()
     {
         if (currentSentences.Count == 0)
         {
-            EndDialogue();
             return;
         }
 
-        DisplaySentence(currentSentences.Dequeue());
+        var thisSentence = currentSentences.Dequeue();
+
+        DisplayResponse(thisSentence.Responses);
+
+        DisplaySentence(thisSentence);
     }
 
     public void DisplaySentence(Sentence sentence)
     {
         nameText.text = sentence.Name;
         dialogueText.text = sentence.Text;
+    }
+
+    public void DisplayResponse(List<Response> responses)
+    {
+        foreach (var textbox in ResponseBoxes)
+        {
+            textbox.text = "";
+        }
+
+        if (responses != null)
+        {
+            // Will break if more than 3 responses
+            for (int i = 0; i < responses.Count; i++)
+            {
+                ResponseBoxes[i].text = responses[i].Text;
+            }
+        }
     }
 }
